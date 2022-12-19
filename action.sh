c471973cf7d06a228cfd8bdd5ae381b28348b0e5
Darin Critchlow <darin.critchlow@champtitles.com>
2022-12-19 16:41:03 -0700

    Update variable setting

--- action.sh
+++ action.sh
@@ -3,4 +3,4 @@
 set -e
 packages=($(npx lerna changed --all))
 PRIVATE="(PRIVATE)"
-echo "${packages[@]}" | tr ' ' '\n' | sed -e "s/$PRIVATE$//" | xargs
+echo "PACKAGES_CHANGED=${packages[@]}" | tr ' ' '\n' | sed -e "s/$PRIVATE$//" | xargs
