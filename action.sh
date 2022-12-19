8f62db9829bbf5d845b83e3cba918b28e853b956
Maddy Richard <82413819+mrichard-ownum@users.noreply.github.com>
2022-12-19 15:36:33 -0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -3,4 +3,8 @@
 set -e
 packages=($(npx lerna changed --all))
 PRIVATE="(PRIVATE)"
-echo "${packages[@]}" | tr ' ' '\n' | sed -e "s/$PRIVATE$//" | xargs | tr '\n' ' ' | tr ' ' '\n'
+changed=$(echo "${packages[@]}" | tr ' ' '\n' | sed -e "s/$PRIVATE$//" | xargs)
+packagesarr=($changed)
+echo ${packagesarr[@]}
+echo "arr length ${#packagesarr[@]}"
+export PACKAGES_CHANGED="$(echo "${packages[@]}" | tr ' ' '\n' | sed -e "s/$PRIVATE$//" | xargs)"
