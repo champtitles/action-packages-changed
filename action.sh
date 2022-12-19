c46243a9e590b0787199ce988aeef58ca083bcb0
Maddy Richard <82413819+mrichard-ownum@users.noreply.github.com>
2022-12-19 13:55:27 -0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 
 set -e
-packages=($(npx lerna run deploy --since HEAD~1))
-suffix=":deploy"
-package=${string#"$suffix"}
-echo "${packages[@]}" | tr ' ' '\n' | grep ".*:deploy" | sed -e "s/$suffix$//"
+packages=($(npx lerna changed --all))
+PRIVATE="(PRIVATE)"
+echo "${packages[@]}" | tr ' ' '\n' | sed -e "s/$PRIVATE$//" | xargs | tr '\n' ' ' | tr ' ' '\n'
